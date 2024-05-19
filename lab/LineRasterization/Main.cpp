2ddea0a63ce3b8210879c24f1a860208f9b4e23c
Seyedmasih Tabaei <seyedmasih.tabaei@stud.hs-coburg.de>
2024-05-19 21:19:12 +0200

    Implemented the base version of naive linear function sampling

--- lab/LineRasterization/Main.cpp
+++ lab/LineRasterization/Main.cpp
@@ -5,11 +5,17 @@
 
 int main()
 {
+	simulateTheLineUsingNaiveLinearFunctionSampling(4, 3);
 }
 
 void simulateTheLineUsingNaiveLinearFunctionSampling(
 	int endPointXCoordinate,
 	int endPointYCoordinate)
 {
-
+	for (int x = 0; x <= endPointXCoordinate; x++)
+	{
+		double continousY = ((double) endPointYCoordinate / endPointXCoordinate) * x;
+		int discreteY = round(continousY);
+		std::cout << "x: " << x << " y: " << discreteY << std::endl;
+	}
 }
