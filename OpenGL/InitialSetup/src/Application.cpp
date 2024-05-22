b12b95ef8b08e3ae182cc29c61037db92d221713
Seyedmasih Tabaei <seyedmasih.tabaei@stud.hs-coburg.de>
2024-05-22 04:59:49 +0200

    Took care of first two steps of rendering pipeline (1. providing the data 2. issuing a draw call)

--- OpenGL/InitialSetup/src/Application.cpp
+++ OpenGL/InitialSetup/src/Application.cpp
@@ -31,24 +31,56 @@ int main(void)
 
     std::cout << "Your OpenGL version: " << glGetString(GL_VERSION) << std::endl;
 
+
+    float positionsOfVertices[6] = 
+    {
+        -0.5f, -0.5f,
+         0.0f,  0.5f,
+         0.5f, -0.5f
+    };
+    // Generating one buffer for the vertices of our triangle
+    unsigned int triangleVerticesBufferId;
+    glGenBuffers(1, &triangleVerticesBufferId);
+
+    // Binding the generated buffer
+    glBindBuffer(GL_ARRAY_BUFFER, triangleVerticesBufferId);
+
+    // Providing the data
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positionsOfVertices, GL_STATIC_DRAW);
+
+
+    // Specifying the layout of our vertex buffer
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+
+    // Enabling the vertex array containing the positions of vertices
+    glEnableVertexAttribArray(triangleVerticesBufferId);
+
+
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         // Drawing a traingle using legacy OpenGL-code
         glClear(GL_COLOR_BUFFER_BIT);
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.0f, 0.5f);
-        glVertex2f(0.5f, -0.5f);
-        glEnd();
+       // glBegin(GL_TRIANGLES);
+       // glVertex2f(-0.5f, -0.5f);
+       // glVertex2f(0.0f, 0.5f);
+       // glVertex2f(0.5f, -0.5f);
+       // glEnd();
 
         // Drawing a point using legacy OpenGL-Code
-        glPointSize(10.0);
-        glColor3f(0, 1, 0);
-        glBegin(GL_POINTS);
-        glVertex2f(0.8f, 0.8f);
-        glEnd();
+        // glPointSize(10);
+        // glColor3f(0, 1, 0);
+        // for (int i = 0; i < 199; i++)
+        // {
+        //    glBegin(GL_POINTS);
+        //    glVertex2f(-1.0f, -1 + i * 0.01f);
+        //    glEnd();
+        // }
+
+        // Issuing a draw call
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
